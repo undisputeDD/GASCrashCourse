406ea8cbd19678a97b2a932284ead94a0d829ed0
Andrii Kreidun <kreyduna@gmail.com>
2026-04-02 12:10:59 +0300

    add base character class

---
+++ Source/GASCrashCourse/Private/CC_BaseCharacter.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CC_BaseCharacter.h"
+
+ACC_BaseCharacter::ACC_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	// Tick and refresh bone transforms whether rendered or not - for bone updates on a dedicated server
+	GetMesh()->VisibilityBasedAnimTickOption = EVisibilityBasedAnimTickOption::AlwaysTickPoseAndRefreshBones;
+}
---
+++ Source/GASCrashCourse/Public/CC_BaseCharacter.h
@@ -0,0 +1,16 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CC_BaseCharacter.generated.h"
+
+UCLASS()
+class GASCRASHCOURSE_API ACC_BaseCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ACC_BaseCharacter();
+};
