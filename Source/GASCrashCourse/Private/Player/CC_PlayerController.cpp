78ada647d0c6d4121bde5db7a5991668342775bb
Andrii Kreidun <kreyduna@gmail.com>
2026-04-03 12:33:06 +0300

    adding default inputs realisation

--- Source/GASCrashCourse/Private/Player/CC_PlayerController.cpp
+++ Source/GASCrashCourse/Private/Player/CC_PlayerController.cpp
@@ -3,6 +3,8 @@
 
 #include "Player/CC_PlayerController.h"
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "GameFramework/Character.h"
 
 void ACC_PlayerController::SetupInputComponent()
 {
@@ -17,25 +19,50 @@ void ACC_PlayerController::SetupInputComponent()
 		InputSubsystem->AddMappingContext(Context, 0);
 	}
 
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
 
+	if (!IsValid(EnhancedInputComponent)) return;
+
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Started, this, &ThisClass::Jump);
+	EnhancedInputComponent->BindAction(JumpAction, ETriggerEvent::Completed, this, &ThisClass::StopJumping);
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::Move);
+	EnhancedInputComponent->BindAction(LookAction, ETriggerEvent::Triggered, this, &ThisClass::Look);
 }
 
 void ACC_PlayerController::Jump()
 {
-	
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->Jump();
 }
 
 void ACC_PlayerController::StopJumping()
 {
-	
+	if (!IsValid(GetCharacter())) return;
+
+	GetCharacter()->StopJumping();
 }
 
 void ACC_PlayerController::Move(const FInputActionValue& Value)
 {
-	
+	if (!IsValid(GetPawn())) return;
+
+	const FVector2D MovementVector = Value.Get<FVector2D>();
+
+	// Find which way is forward
+	const FRotator YawRotation(0.f, GetControlRotation().Yaw, 0.f);
+
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	GetPawn()->AddMovementInput(ForwardDirection, MovementVector.Y);
+	GetPawn()->AddMovementInput(RightDirection, MovementVector.X);
 }
 
 void ACC_PlayerController::Look(const FInputActionValue& Value)
 {
-	
+	const FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	AddYawInput(LookAxisVector.X);
+	AddYawInput(LookAxisVector.Y);
 }
