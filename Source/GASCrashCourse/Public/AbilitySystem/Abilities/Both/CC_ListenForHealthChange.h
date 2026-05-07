324740ce2649dc5c25de0d24252748a20aca22a7
Andrii Kreidun <kreyduna@gmail.com>
2026-05-07 13:49:53 +0300

    adding death ability and montage also adding task for health change track

---
+++ Source/GASCrashCourse/Public/AbilitySystem/Abilities/Both/CC_ListenForHealthChange.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/CC_GameplayAbility.h"
+#include "CC_ListenForHealthChange.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GASCRASHCOURSE_API UCC_ListenForHealthChange : public UCC_GameplayAbility
+{
+	GENERATED_BODY()
+	
+};
